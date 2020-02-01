9bf7b8a724fc22dac2152a7762ebcd4d922ca966
dani123yal <daniyal.safdar7@gmail.com>
2020-02-01 17:36:22 +0500

    added mirror networking assets

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class MyNetworkManager : NetworkManager
+{
+    private NetworkManager networkManager;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MyStartHost()
+    {
+        Debug.Log("Starting host at: " + Time.timeSinceLevelLoad);
+        StartHost();
+    }
+
+    public override void OnStartHost()
+    {
+        Debug.Log("host started at:" + Time.timeSinceLevelLoad);
+    }
+}
